3f8c2731bf00e64dcdb4dbca928cc461d28f4f36
bodV6 <fairybod@gmail.com>
2023-02-08 20:45:10 +0100

    completed functionality

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const contentDisplay = document.querySelector('.content-container');
+
+const thankYouDisplay = document.querySelector('.thankyou-container');
+
+const totalRating = document.querySelector('ul');
 const ratingPicker = document.querySelector(".rating-container");
 
-ratingPicker.addEventListener("click", (e) => console.log(e.target.value));
+const selectionMessage = document.querySelector('.selected-rating')
+
+
+ratingPicker.addEventListener("click", clickedNumber, false);
+
+
+
+
+
+
+function clickedNumber(e) {
+    
+    contentDisplay.style.display = "none";
+
+    thankYouDisplay.style.display = "flex";
+
+    let message = document.createElement('p');
+
+    message.innerText = `You selected ${e.target.text} out of ${totalRating.children.length}`;
+    
+    selectionMessage.appendChild(message);
+}
